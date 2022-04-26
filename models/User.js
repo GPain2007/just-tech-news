1b8476b5523b1631e658d0d6ea97e6ccf2ea21e3
Govnor Payne <govnorpayne@gmail.com>
2022-04-26 15:42:36 -0500

    added password protection

--- models/User.js
+++ models/User.js
@@ -1,5 +1,21 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
+const bcrypt = require("bcrypt");
+// const saltRounds = 10;
+// const myPlaintextPassword = "s0//P4$$w0rD";
+// const someOtherPlaintextPassword = "not_bacon";
+
+// bcrypt.hash(myPlaintextPassword, saltRounds, function (err, hash) {
+//   // Store hash in your password DB.
+// });
+
+// // Load hash from your password DB.
+// bcrypt.compare(myPlaintextPassword, hash, function (err, result) {
+//   result == true;
+// });
+// bcrypt.compare(someOtherPlaintextPassword, hash, function (err, result) {
+//   result == false;
+// });
 
 // create our User model
 class User extends Model {}
@@ -65,6 +81,21 @@ User.init(
     },
   },
   {
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      }, // set up beforeUpdate lifecycle "hook" functionality
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
+        return updatedUserData;
+      },
+    },
+
     sequelize,
     timestamps: false,
     freezeTableName: true,
